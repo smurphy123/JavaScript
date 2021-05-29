819fb82a05d0aae06ff9e0fbea3eabe807c48b1e
smurphy123 <smurphysmail@gmail.com>
2021-05-29 17:53:47 -0400

    Repo Update

--- DynamicProgramming/canSum.js
+++ DynamicProgramming/canSum.js
@@ -1,13 +1,16 @@
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return true;
     if (targetSum < 0) return false;
 
     for (let num of numbers) {
         const remainder = targetSum - num;
-        if (canSum(remainder, numbers) === true) {
+        if (canSum(remainder, numbers, memo) === true) {
+            memo[targetSum] = true;
             return true;
         }
     }
+    memo[targetSum] = false;
     return false;
 };
 
