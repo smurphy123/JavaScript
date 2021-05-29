32959e3829064146b1674e467bbd7c49180efefa
smurphy123 <smurphysmail@gmail.com>
2021-05-29 14:57:16 -0400

    Repo Update

--- DynamicProgramming/fib.js
+++ DynamicProgramming/fib.js
@@ -1,7 +1,14 @@
-const fib = (n) => {
+// memoization
+// js object, keys will be arg to fn, value will be the return value
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
     if (n <= 2) return 1;
-    return fib(n-1) + fib(n-2);
+    memo[n] = fib(n-1, memo) + fib(n-2, memo);
+    return memo[n];
 };
 
 console.log(fib(6));
+console.log(fib(7));
 console.log(fib(8));
+console.log(fib(50));
