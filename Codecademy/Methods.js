db484fd68dd3d1a8c7bb0553574c7ff0779b3025
smurphy123 <smurphysmail@gmail.com>
2021-08-26 13:28:45 -0400

    Repo Update

---
+++ Codecademy/Methods.js
@@ -0,0 +1,8 @@
+console.log('hello'.toUpperCase()); // Prints 'HELLO'
+console.log('Hey'.startsWith('H')); // Prints true
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
