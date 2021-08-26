0f099a07ff2882eb7645324a4630b64dd5d224a1
smurphy123 <smurphysmail@gmail.com>
2021-08-26 13:26:21 -0400

    Repo Update

---
+++ Codecademy/ArithmeticOperators.js
@@ -0,0 +1,12 @@
+console.log(3 + 4); // Prints 7
+console.log(5 - 1); // Prints 4
+console.log(4 * 2); // Prints 8
+console.log(9 / 3); // Prints 3
+
+console.log(11 % 3); // Prints 2
+console.log(12 % 3); // Prints 0
+
+console.log(7 + 3.5);
+console.log(2019-1969);
+console.log(65/240);
+console.log(0.2708 * 100);
