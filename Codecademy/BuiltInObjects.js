7e7eff9363a6a0784e9ed46dff10760b1176ea64
smurphy123 <smurphysmail@gmail.com>
2021-08-26 13:30:00 -0400

    Repo Update

---
+++ Codecademy/BuiltInObjects.js
@@ -0,0 +1,6 @@
+console.log(Math.random()); // Prints a random number between 0 and 1
+console.log(Math.floor(Math.random() * 50)); // Prints a random whole number between 0 and 50
+
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
