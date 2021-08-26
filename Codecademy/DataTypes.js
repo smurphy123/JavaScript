452d03b487a9fa0d038cbfc033000879ccb72631
smurphy123 <smurphysmail@gmail.com>
2021-08-26 13:25:12 -0400

    Repo Update

--- Codecademy/DataTypes.js
+++ Codecademy/DataTypes.js
@@ -2,3 +2,6 @@ console.log('JavaScript');
 console.log(2011);
 console.log('Woohoo! I love to code! #codecademy');
 console.log(20.49);
+
+console.log('Location of Codecademy headquarters: 575 Broadway, New York City');
+console.log(40);
