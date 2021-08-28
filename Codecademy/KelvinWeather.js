e15089cf42d347a2a157333f9a1e64140682d02f
smurphy123 <smurphysmail@gmail.com>
2021-08-27 21:10:05 -0400

    Repo Update

---
+++ Codecademy/KelvinWeather.js
@@ -0,0 +1,7 @@
+const kelvin = 0; // constant value for kelvin value
+const celsius = kelvin - 273; // constant value for celcius value
+
+// Fahrenheit conversion
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
+// Prin fahrenheit temperature
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
