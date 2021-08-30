7fdeef26f93d34e59f2845d66c594153fe4cd265
smurphy123 <smurphysmail@gmail.com>
2021-08-29 21:19:11 -0400

    Repo Update

---
+++ Codecademy/StopLight.js
@@ -0,0 +1,11 @@
+let stopLight = 'yellow';
+ 
+if (stopLight === 'red') {
+  console.log('Stop!');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down.');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
