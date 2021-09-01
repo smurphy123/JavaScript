5220bce00fdff45acbef97dcaceaee84dbab1ab9
smurphy123 <smurphysmail@gmail.com>
2021-08-31 21:32:27 -0400

    Repo Update

---
+++ Codecademy/MagicEightBall.js
@@ -0,0 +1,39 @@
+var userName = "";
+
+userName ? 'Hello, ' + userName + '!' : 'Hello!';
+
+userQuestion = 'Will I pass the coding interview?';
+console.log(userQuestion);
+
+randomNumber = Math.floor(Math.random() * 8);
+
+eightBall = "";
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(eightBall);
